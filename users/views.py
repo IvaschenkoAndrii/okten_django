42317e417d830835b83f2a539a8bff35df292d69
Андрей <ivaschenko84@gmail.com>
2022-11-23 15:10:05 +0100

    lesson1_django

--- users/views.py
+++ users/views.py
@@ -18,7 +18,6 @@ g = gen()
 class UserLisrCreate(APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.id=self.gen()
 
     def gen(self):
         count = 0
@@ -39,9 +38,29 @@ class UserLisrCreate(APIView):
         user = {'id': id_user, 'name': self.request.data['name'], 'age': self.request.data['age']}
         users.append(user)
         print(users)
+
         try:
             with open('users/users.json', 'w') as file:
                 json.dump(users, file)
         except Exception as err:
             print(err)
+        return Response(user)
+
+
+class UserRetrieveUpdateDelete(APIView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.users = users
+
+    def get(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        try:
+            with open('users/users.json', 'r') as file:
+                self.users = json.load(file)
+                try:
+                    user=self.users[pk-1]
+                except IndexError:
+                    return Response ('Not Found')
+        except Exception as err:
+            print('not found')
         return Response(user)
