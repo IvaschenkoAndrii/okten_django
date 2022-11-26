38f3b0098dd07bb9c8566cf23e9f1c0952900105
Андрей <ivaschenko84@gmail.com>
2022-11-26 21:44:03 +0100

    lesson2_django

--- apps/cars/serializers.py
+++ apps/cars/serializers.py
@@ -8,4 +8,3 @@ class CarSerializer(ModelSerializer):
         model = CarModel
         # fields = '__all__'
         fields = ('id','name', 'age', 'seats', 'body_type','engine_volume')
-        # fields = ('id','name', 'age')  #при виведені всіх машин показувати тільки (id, марку машини та рік)
