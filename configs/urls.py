37ff9a3c8cc83eaf19105f74f21de4d8f62d1fd2
Андрей <ivaschenko84@gmail.com>
2022-11-27 10:29:40 +0100

    lesson2_django

--- configs/urls.py
+++ configs/urls.py
@@ -1,8 +1,5 @@
-from django.urls import path
-from apps.cars.views import CarsListCreateView, CarRetrieveUpdateDestroyView
+from django.urls import path, include
 
 urlpatterns = [
-    path('cars',CarsListCreateView.as_view()),
-    path('cars/<int:pk>',CarRetrieveUpdateDestroyView.as_view())
+    path('cars', include('apps.cars.urls'))
 ]
-
