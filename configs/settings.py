82e0c10d78b9f0e5e7dce0717a3948fbba0c63a8
Андрей <ivaschenko84@gmail.com>
2022-11-30 11:38:01 +0100

    lesson2_django

--- configs/settings.py
+++ configs/settings.py
@@ -123,9 +123,6 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ]
 }
 
 CORS_ALLOWED_ORIGINS = [
