ffe3ecfedc09279c213ef864dba16114c66dd78e
Андрей <ivaschenko84@gmail.com>
2022-12-03 15:28:09 +0100

    HW4

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserCreateView
+from .views import UserActivationView, UserCreateView
 
 urlpatterns = [
-    path('', UserCreateView.as_view())
+    path('', UserCreateView.as_view()),
+    path('/<int:pk>', UserActivationView.as_view())
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,39 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-from .permissions import IsSuperUser
+from .models import UserModel
+from .permissions import IsStaff, IsSuperUser
 from .serializers import UserSerializer
 
 
 class UserCreateView(CreateAPIView):
     serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
     permission_classes = (IsSuperUser,)
+
+
+class UserActivationView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
+    permission_classes = (AllowAny,)
+
+    def patch(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        data = self.request.data
+        user = UserModel.objects.get(pk=pk)
+        serializer = UserSerializer(user, data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+
+    # def post(self, request, *args, **kwargs):
+    #     pk = kwargs.get('pk')
+    #     data=self.request.data
+    #     user = UserModel.objects.filter(id=pk)
+    #     serializer = UserSerializer(user)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save(user=user)
+    #     print(serializer.data)
+    #     return Response(serializer.data)
