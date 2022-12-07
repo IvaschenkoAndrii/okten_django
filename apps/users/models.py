b6b2ba9d345135a71aabc1a0fb902ffdc1a71bc3
Андрей <ivaschenko84@gmail.com>
2022-12-07 12:02:46 +0100

    5

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.db import models
 from django.core import validators as V
+from django.db import models
 
+from .enums import RegEx
 from .managers import UserManager
 
 
@@ -10,7 +11,9 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
         db_table = 'auth_user'
 
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=128,validators=[
+        V.RegexValidator(RegEx.PASSWORD.pattern,RegEx.PASSWORD.msg)
+    ])
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,8 +28,14 @@ class ProfileModel(models.Model):
     class Meta:
         db_table = 'profile'
 
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, validators=[
+        V.RegexValidator(RegEx.NAME.pattern, RegEx.NAME.msg)
+    ])
+    surname = models.CharField(max_length=20, validators=[
+        V.RegexValidator(RegEx.NAME.pattern, RegEx.NAME.msg)
+    ])
     age = models.IntegerField(validators=[V.MinValueValidator(18), V.MaxValueValidator(150)])
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=10,validators=[
+        V.RegexValidator(RegEx.PHONE.pattern,RegEx.PHONE.msg)
+    ])
     user = models.OneToOneField(UserModel, on_delete=models.CASCADE, related_name='profile')
