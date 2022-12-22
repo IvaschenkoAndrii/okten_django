f49b184bbc3ea9e7b1dc00cae4148472ddfb0183
Андрей <ivaschenko84@gmail.com>
2022-12-22 13:27:33 +0100

    9_2 React

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+const App = () => {
+
+    const[cars, setCars]=useState([])
+
+  useEffect(()=>{
+    axios.get('/api.cars').then(value => setCars(value.data))
+      },
+      [])
+    return (
+        <div>
+            {cars.map(car=><div key={car.id}>car.name</div>)}
+        </div>
+    );
+};
+
+export default App;
+
